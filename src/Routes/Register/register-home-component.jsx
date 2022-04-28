e49f7605c8f7da63adff9ae37f3b3e3b6ca78b9e
Vayre047 <94454907+Vayre047@users.noreply.github.com>
2022-04-28 17:02:39 +0100

    Implementação da background image

--- src/Routes/Register/register-home-component.jsx
+++ src/Routes/Register/register-home-component.jsx
@@ -1,11 +1,9 @@
-import RegisterImage from '../../Components/Register Configuration/register-image-component';
 import RegisterText from '../../Components/Register Configuration/register-text-component';
 import './register-home-component.scss'
 
 function Register(){
     return(
         <div>
-            {/*<RegisterImage />*/}
             <RegisterText />
         </div>
     )
