e49fc3f20f7d25465075e9446d40b53554f9c61f
Vayre047 <94454907+Vayre047@users.noreply.github.com>
2022-05-19 19:08:05 +0100

    Ajustes Finais de CSS

--- src/Context/user-context.jsx
+++ src/Context/user-context.jsx
@@ -10,10 +10,7 @@ export const UserProvider = ({ children }) => {
     const [ currentUser, setCurrentUser ] = useState({});
     const [ isLoggedIn, setIsLoggedIn ] = useState( false );
     const [width, setWidth] = useState(window.innerWidth);
-
-    /*const login =(email, password) => {
-        setLogin(true);
-    }*/
+    
     const value = { currentUser, setCurrentUser, isLoggedIn, setIsLoggedIn, width, setWidth };
 
     return <UserContext.Provider value={value}>{ children }</UserContext.Provider>
